851c6d67ef5dcd63155681c361c7590799dbcffa
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2023-04-10 15:45:50 -0300

    inserido hideKeyboard e imc com 2 casas decimais

--- app/src/main/java/com/ramalho/calculadora_imc_kotlin/MainActivity.kt
+++ app/src/main/java/com/ramalho/calculadora_imc_kotlin/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ramalho.calculadora_imc_kotlin
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,22 +17,25 @@ class MainActivity : AppCompatActivity() {
         val altura = findViewById<EditText>(R.id.Altura)
         val resultado = findViewById<TextView>(R.id.Resultado)
         val calcular = findViewById<Button>(R.id.Calcular)
+        val hideKeyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         calcular.setOnClickListener {
             try {
                 val pesoD = peso.text.toString().toDouble()
                 val alturaD = altura.text.toString().toDouble()
 
-                val imc = pesoD / (alturaD * alturaD)
+                val imcGrande = pesoD / (alturaD * alturaD)
+                val imc = String.format("%.2f", imcGrande).toDouble()
+                hideKeyboard.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
                 if (imc < 18.5) {
-                    resultado.text = "Abaixo do peso"
+                    resultado.text = "Seu IMC é: $imc \nAbaixo do peso"
                 } else if (imc < 25) {
-                    resultado.text = "Peso normal"
+                    resultado.text = "Seu IMC é: $imc \nPeso normal"
                 } else if (imc < 30) {
-                    resultado.text = "Sobrepeso"
+                    resultado.text = "Seu IMC é: $imc \nSobrepeso"
                 } else {
-                    resultado.text = "Obesidade"
+                    resultado.text = "Seu IMC é: $imc \nObesidade"
                 }
             } catch (e: NumberFormatException) {
                 resultado.text = "Insira um valor válido"
